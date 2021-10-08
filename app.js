a08ccc7b3b9648841e24dc044c00402b786c54d7
smilingfroggy <smilingfroggy@hotmail.com>
2021-10-08 15:15:38 +0800

    feat: setup handlebars, add login page

--- app.js
+++ app.js
@@ -1,2 +1,23 @@
 const express = require("express")
 const app = express()
+const exphbs = require('express-handlebars')
+
+//mongoose + model
+//import from seeder
+
+// template engine
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+// routes
+app.get("/", (req, res) => {
+  res.render('login')
+})
+
+app.post('/login', (req, res) => {
+  // check input
+})
+
+app.listen(3000, () => {
+  console.log('App is running on http://localhost:3000')
+})
